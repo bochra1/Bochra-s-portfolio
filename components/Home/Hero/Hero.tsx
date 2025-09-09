95e82b531dd798ea5b2bd72956c1791b09cbb116
Bochra Labbene <bochralb01@gmail.com>
2025-09-09 19:45:02 +0200

    updates

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -29,7 +29,7 @@ const Hero = () => {
              <a
               href="/BochraLabbene_cv.pdf"  // Link to the CV file in the public directory
               download="Bochra_Labbene_CV.pdf"  // Specify the filename for the download
-              className="w-52 md:px-8 md:py-2.5 px-6 py-1.5 text-white font-semibold text-sm transition-all duration-200 rounded-lg mt-8 bg-blue-700 hover:bg-blue-900 flex items-center space-x-z"
+              className="hidden w-52 md:px-8 md:py-2.5 px-6 py-1.5 text-white font-semibold text-sm transition-all duration-200 rounded-lg mt-8 bg-blue-700 hover:bg-blue-900 flex items-center space-x-z"
             >              <span>Download CV</span>
               <FaDownload /> 
             </a>
